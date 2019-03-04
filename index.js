363dafeb89e416dea6d2363bf9eba14babbd93a6
leaf-junkie <ragsdale.jar@gmail.com>
2019-03-04 17:16:43 -0500

    game logic

--- index.js
+++ index.js
@@ -19,12 +19,10 @@ inquirer.prompt([
         choices: ['New game', 'Exit']
     }
 ]).then((response) => {
-    
-    console.log("hi");
-    // if (response.begin.choices[0]) {
-    //     console.log(`Welcome to Word Guess, ${response.username}`);
-    //     startGame();
-    // } else return;
+    if (response.begin === 'New game') {
+        console.log(`Welcome to Word Guess, ${response.username}`);
+        playGame();
+    } else return;
 });
 
 const dogBreeds = [
@@ -88,72 +86,74 @@ const dogBreeds = [
     'Yorkshire Terrier'
 ];
 
-// TODO:Randomly selects a word and uses the Word constructor to store it
-// 'Randomly' select word from array
-const randomIndex = Math.floor(Math.random() * dogBreeds.length);
-const randomWord = dogBreeds[randomIndex];
+function playGame() {
+    // TODO:Randomly selects a word and uses the Word constructor to store it
+    // 'Randomly' select word from array
+    const randomIndex = Math.floor(Math.random() * dogBreeds.length);
+    const randomWord = dogBreeds[randomIndex];
+    let winCount = 0;
+    let loseCount = 0;
+    
+    // Move guessed letters into another array
+    let letters = 'abcdefghijklmnopqrstuvwxyz ';
+    letters = letters.toUpperCase();
+    let guessed = [];
 
-console.log(`The selected word is: ${randomWord} (Index ${randomIndex})`);
+    // TODO:Prompts the user for each guess and keeps track of the user's remaining guesses
+    // Begin with 10 guesses
+    let guessesRemaining = 10;
 
-// Move guessed letters into another array
-let letters = 'abcdefghijklmnopqrstuvwxyz ';
-letters = letters.toUpperCase();
-let guessed = [];
+    function keyUpHandler(event) {
+        // Filter characters that are not letters or spaces
+        if (!letters.includes(event.key.toUpperCase())) return;
+    
+        // Ignore redundant input
+        if (guessed.includes(event.key)) {
+            return;
+        } else {
+            guessed.push(event.key);
+        }
+        console.log(`${guessed}`);
+    };
 
-// TODO:Prompts the user for each guess and keeps track of the user's remaining guesses
-// Begin with 10 guesses
-let guessesRemaining = 10;
-
-function keyUpHandler(event) {
-    // Filter characters that are not letters or spaces
-    if (!letters.includes(event.key.toUpperCase())) return;
-
-    // Ignore redundant input
-    if (guessed.includes(event.key)) {
-        return;
+    // Check if pressed letter is included in name of current plant 
+    if (checkLetter(event.key)) {
+        // had the letter
+        
     } else {
-        guessed.push(event.key);
+        // did not have the letter
+    
+        // Decrement guess count
+        guessesRemaining -= 1;
+        
+        // Display the number of remaining guesses under 'Guesses Remaining'
+        console.log('Number of guesses remaining: ' + guessesRemaining);
+        document.getElementById('guessesRemaining').innerHTML = guessesRemaining;
     }
-    console.log(`${guessed}`);
-};
-
-// Check if pressed letter is included in name of current plant 
-if (checkLetter(event.key)) {
-    // had the letter
-    
-} else {
-    // did not have the letter
-
-    // Decrement guess count
-    guessesRemaining -= 1;
-    
-    // Display the number of remaining guesses under 'Guesses Remaining'
-    console.log('Number of guesses remaining: ' + guessesRemaining);
-    document.getElementById('guessesRemaining').innerHTML = guessesRemaining;
 }
 
-// // Win
-// function win() {
-//     let wins;
-//     wins += 1;
-//     console.log('You win!');
+function win() {
+    let wins;
+    wins += 1;
+    console.log('You win!');
 
-//     //TODO: Could add new inquirer question to ask if user would like to play again
-// };
+    //TODO: Could add new inquirer question to ask if user would like to play again
+};
 
-// function lose() {
-//     let losses;
-//     losses -= 1;
-//     console.log('You lost \nGAME OVER');
+function lose() {
+    let losses;
+    losses -= 1;
+    console.log('G A M E   O V E R \nYou lose');
 
-//     //TODO: Could add new inquirer question to ask if user would like to play again
-// };
+    //TODO: Could add new inquirer question to ask if user would like to play again
+};
 
-// function reset() {
-//     guessesRemaining = 10;
-//     guessed = [];
-//     randomIndex = Math.floor(Math.random() * dogBreeds.length);
-//     randomWord = dogBreeds[randomIndex];
-//     console.log(`Reset. New word: ${randomWord}`);
-
-// }
+function reset() {
+    winCount = 0;
+    loseCount = 0;
+    guessesRemaining = 10;
+    guessed = [];
+    randomIndex = Math.floor(Math.random() * dogBreeds.length);
+    randomWord = dogBreeds[randomIndex];
+    console.log('Game reset');
+}
